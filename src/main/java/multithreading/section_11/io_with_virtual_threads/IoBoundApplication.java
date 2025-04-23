62b26d72d3dc1fb404899a2db1a4d3777a2164e9
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2025-04-23 22:03:46 +0200

    IO with Virtual Thread

---
+++ src/main/java/multithreading/section_11/io_with_virtual_threads/IoBoundApplication.java
@@ -0,0 +1,44 @@
+package multithreading.section_11.io_with_virtual_threads;
+
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class IoBoundApplication {
+    public static final int NUMBER_OF_THREADS = 10_000;
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Press enter to start");
+        s.nextLine();
+
+        System.out.printf("Running %d tasks\n", NUMBER_OF_THREADS);
+
+        long start = System.currentTimeMillis();
+        performTask();
+        System.out.printf("Tasks took %dms to compete\n", System.currentTimeMillis() - start);
+    }
+
+    private static void performTask() {
+        ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor();
+        for (int i = 0; i < NUMBER_OF_THREADS; i++) {
+            executorService.execute(new Runnable() {
+                @Override
+                public void run() {
+                    for (int j = 0; j < 100; j++) {
+                        blockingIoOperation();
+                    }
+                }
+            });
+        }
+    }
+
+    public static void blockingIoOperation() {
+        System.out.println("Execution a blocking task: " + Thread.currentThread());
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException interruptedException) {
+            throw new RuntimeException();
+        }
+    }
+}
